1c34a0a990be446d907e009cbe3cb4de6ef620f3
Bing-Qian Luan <bingqian.luan@teleyi.com>
2017-01-01 20:43:23 +0800

    aliyun mirror

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,10 @@ timedatectl set-timezone Asia/Shanghai
 
 # Use apt sources from China
 cat > /etc/apt/sources.list <<-EOF
-deb http://cn.archive.ubuntu.com/ubuntu xenial main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu xenial-updates main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse
-deb http://security.ubuntu.com/ubuntu xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
 EOF
-apt-get clean
+apt-get update
